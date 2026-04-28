7169ac06499c86cd487fad33d7baa7905173c573
Emranfaishal <emranfaishalrahat@gmail.com>
2026-04-28 22:06:43 +0600

    photoDatails add

--- src/components/PhotoCard.jsx
+++ src/components/PhotoCard.jsx
@@ -1,5 +1,6 @@
 import { Button, Card, Chip } from "@heroui/react";
 import Image from "next/image";
+import Link from "next/link";
 import { BiDownload } from "react-icons/bi";
 import { FaHeart } from "react-icons/fa";
 
@@ -26,7 +27,9 @@ const PhotoCard = ({ photo }) => {
                     <p>{photo.downloads}</p>
                 </div>
             </div>
-            <Button variant="outline" className={'w-full'}>View</Button>
+            <Link href={`/all-photos/${photo.id}`}>
+                <Button variant="outline" className={'w-full'}>View</Button>
+            </Link>
         </Card>
     );
 };
